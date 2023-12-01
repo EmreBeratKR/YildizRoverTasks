b9f9347ffc23ec6bf2f00c0ea94e40534077ca72
EmreBeratKR <emreberatkrn@gmail.com>
2023-12-01 08:39:42 +0300

    refactor

--- Task 3.2 - Detecting Labels/label_detector.py
+++ Task 3.2 - Detecting Labels/label_detector.py
@@ -1,22 +1,23 @@
 def contains_label(pixels: list[list[int]]) -> bool:
     for j in range(1, len(pixels) - 1):
         for i in range(1, len(pixels[j]) - 1):
-            if pixels[i][j] == 0:
-                if pixels[i-1][j-1] != 255:
-                    continue
-                if pixels[i][j-1] != 255:
-                    continue
-                if pixels[i+1][j-1] != 255:
-                    continue
-                if pixels[i-1][j] != 255:
-                    continue
-                if pixels[i+1][j] != 255:
-                    continue
-                if pixels[i-1][j+1] != 255:
-                    continue
-                if pixels[i][j+1] != 255:
-                    continue
-                if pixels[i+1][j+1] != 255:
-                    continue
-                return True
+            if pixels[i][j] != 0:
+                continue
+            if pixels[i-1][j-1] != 255:
+                continue
+            if pixels[i][j-1] != 255:
+                continue
+            if pixels[i+1][j-1] != 255:
+                continue
+            if pixels[i-1][j] != 255:
+                continue
+            if pixels[i+1][j] != 255:
+                continue
+            if pixels[i-1][j+1] != 255:
+                continue
+            if pixels[i][j+1] != 255:
+                continue
+            if pixels[i+1][j+1] != 255:
+                continue
+            return True
     return False
