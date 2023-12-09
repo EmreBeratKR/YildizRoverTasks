a70fe5c67ba79da102ceda208755c19ec630c2fb
EmreBeratKR <emreberatkrn@gmail.com>
2023-12-10 02:10:21 +0300

    Task 4.1 refactor main.py

--- Task 4/Task 4.1 - Basic Neural Network/main.py
+++ Task 4/Task 4.1 - Basic Neural Network/main.py
@@ -10,12 +10,20 @@ TRAIN_ITERATION_COUNT = 1000
 LEARN_RATE = 0.1
 
 
+def train_neural_network(neural_network: BasicNeuralNetwork) -> None:
+    for i in range(TRAIN_ITERATION_COUNT):
+        neural_network.train(train_data_set, LEARN_RATE)
+
+
+def test_neural_network(neural_network: BasicNeuralNetwork) -> None:
+    neural_network.test(train_data_set)
+
+
 def main():
     random.seed(SEED)
     network = BasicNeuralNetwork(INPUT_COUNT)
-    for i in range(TRAIN_ITERATION_COUNT):
-        network.train(train_data_set, LEARN_RATE)
-    network.test(train_data_set)
+    train_neural_network(network)
+    test_neural_network(network)
 
 
 if __name__ == '__main__':
